557ff75e954999811eef62072911451794c24b18
SulaimanEfendi <snk_efendi@hotmail.com>
2021-09-30 23:20:44 +0300

    This task done by writting a modified code

--- task.js
+++ task.js
@@ -1,44 +1,57 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
+// This program read a data.txt file then sort the array in the file as ASC and save it in output.txt file
+var fs = require('fs');
 
-console.log("Original numbers list: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] > numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
+try {  
+    var data = fs.readFileSync('data.txt', 'utf8'); // open and read the data.txt file
+    data = JSON.parse("[" + data + "]"); // convert the string of the file to array
+    console.log("data.txt file contents array : ", data); // print the data.txt contents
+    data = ASCbubblesorting(data); // call the ASC sort function to sort data.txt file
+    fs.writeFileSync('output.txt', data.toString()); // write the array on output.txt file
+    console.log("The array sorted as ASC order and saved in output.txt file"); // for style 
+} catch(e) {
+    console.log('Error:', e.stack);
 }
 
-console.log("Numbers list After sorting: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
+// This function to sort an array as ASC by bubble algorthim
+function ASCbubblesorting (numbersarray)
 {
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
+    //let numbersarray = [5, 8, 0, 1, 9, 11, 15, 16]; // immutable 
+    let numbers = [...numbersarray]; // copy as mutable array
+    console.log("Original Array Numbers list before ASC order sorting: ", numbersarray);
+    for(let i = 0 ; i < numbers.length ; i++)
     {
-        if(numbers[i] < numbers[j + 1])
+        for(let j = 0 ; j < numbers.length ; j++)
         {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
+            if(numbers[j] > numbers[j + 1])
+            {
+                let temp = numbers[j];
+                numbers[j] = numbers[j + 1]
+                numbers[j + 1] = temp;
+            } 
         }
+        
     }
+    console.log("Numbers list After ASC order sorting: ", numbers);
+    return(numbers);
 }
 
-console.log("Numbers list After Desc sorting: ", numbers)
-
-
+// This function to sort an array as DESC by bubble algorthim
+function DESCbubblesorting (numbersarray)
+{ 
+    let numbers = [...numbersarray]; // copy as mutable array
+    console.log("Original Array Numbers list before DESC sorting: ", numbersarray);
+    for(let i = 0 ; i < numbers.length ; i++)
+    {
+        for(let j = 0 ; j < numbers.length ; j++)
+        {
+            if(numbers[j] < numbers[j + 1])
+            {
+                let temp = numbers[j];
+                numbers[j] = numbers[j + 1]
+                numbers[j + 1] = temp;
+            }
+        }
+    }
+    console.log("Numbers list After DESC order sorting: ", numbers);
+    return(numbers);
+}
